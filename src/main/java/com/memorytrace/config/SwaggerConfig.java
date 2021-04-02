5893ca53aebe473b5261e0547c2e71409869fe85
hye0 <nanda5050@naver.com>
2021-04-02 17:50:34 +0900

    [STYLE] 주석 삭제

--- src/main/java/com/memorytrace/config/SwaggerConfig.java
+++ src/main/java/com/memorytrace/config/SwaggerConfig.java
@@ -17,44 +17,33 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableWebMvc
 public class SwaggerConfig extends WebMvcConfigurerAdapter {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
         System.out.println("******************************Configuring swagger resource handler");
         registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
+            .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
-//        registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
-//        registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
-//    }
-
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo()) //기본정보
-                .select()
-                .apis(RequestHandlerSelectors.any())  //주소접근 옵션
-                .apis(RequestHandlerSelectors.basePackage("com.memorytrace.controller"))
-                .paths(PathSelectors.any())
-                .build();
+            .apiInfo(apiInfo()) //기본정보
+            .select()
+            .apis(RequestHandlerSelectors.any())  //주소접근 옵션
+            .apis(RequestHandlerSelectors.basePackage("com.memorytrace.controller"))
+            .paths(PathSelectors.any())
+            .build();
     }
 
-    /**
-     *  Swagger API INFO
-     * @return
-     */
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("기록이 최고야 API with Swagger")
-                .description("스웨거 테스트")
-                .version("2.0")
-                .build();
+            .title("기록이 최고야 API with Swagger")
+            .description("스웨거 테스트")
+            .version("2.0")
+            .build();
     }
 }
