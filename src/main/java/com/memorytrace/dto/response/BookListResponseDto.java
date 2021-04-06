6fd07e3e5afd8ad39e85fc3ae7d620a337edb452
congcoding <congcoding@gmail.com>
2021-04-06 21:02:02 +0900

    [STYLE] Swagger 수정

--- src/main/java/com/memorytrace/dto/response/BookListResponseDto.java
+++ src/main/java/com/memorytrace/dto/response/BookListResponseDto.java
@@ -1,17 +1,29 @@
 package com.memorytrace.dto.response;
 
 import com.memorytrace.domain.UserBook;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@ApiModel(value = "BookList 조회 요청")
 public class BookListResponseDto {
 
+    @ApiModelProperty(position = 1, required = true)
     private Long bid;
+
+    @ApiModelProperty(position = 2, required = true)
     private String nickname;
+
+    @ApiModelProperty(position = 3, required = true)
     private String title;
+
+    @ApiModelProperty(position = 4, required = true)
     private Byte bgColor;
+
+    @ApiModelProperty(position = 5, required = true)
     private String modifiedDate;
 
     public BookListResponseDto(UserBook entity) {
