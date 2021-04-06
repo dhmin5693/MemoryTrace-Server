0cb15bf9f200f4630c089989c98f3d3eb1abca07
hye0 <nanda5050@naver.com>
2021-04-06 22:10:56 +0900

    [REFACTOR] 코드 리팩토링 , .gitignore추가

--- src/main/java/com/memorytrace/dto/response/UserDetailResponseDto.java
+++ src/main/java/com/memorytrace/dto/response/UserDetailResponseDto.java
@@ -8,21 +8,12 @@ public class UserDetailResponseDto {
 
     private Long uid;
     private String nickName;
-    //    private String snsKey;
     private String profileImg;
-    // private  // 순서
-//    private String createDate;
-//    private LocalDateTime modifiedDate;
-//    private byte isWithdrawal;
 
     public UserDetailResponseDto(User entity) {
         this.uid = entity.getUid();
         this.nickName = entity.getNickname();
-//        this.snsKey = entity.getSnsKey();
         this.profileImg = entity.getProfileImg();
-//        this.createDate = entity.getProfileImg();
-//        this.modifiedDate = entity.getModifiedDate();
-//        this.isWithdrawal = entity.getIsWithdrawal();
     }
 
 }
