5b43aa68b5e9adcd9cf8e55293feb5f93493d066
congcoding <congcoding@gmail.com>
2021-04-06 21:01:29 +0900

    [FIX] Book 생성할 때 isDelete는 default 값 사용하도록 수정 (#7)

--- src/main/java/com/memorytrace/dto/request/BookSaveRequestDto.java
+++ src/main/java/com/memorytrace/dto/request/BookSaveRequestDto.java
@@ -16,36 +16,30 @@ import lombok.NoArgsConstructor;
 public class BookSaveRequestDto {
 
     @NotNull
-    @ApiModelProperty(position = 1, required = true, dataType = "Long", value = "현재 차례인 uid")
+    @ApiModelProperty(position = 1, required = true, value = "현재 차례인 uid (생성자 uid)")
     private Long whoseTurn;
 
     @NotNull
-    @ApiModelProperty(position = 2, required = true, dataType = "String", value = "book 제목")
+    @ApiModelProperty(position = 2, required = true)
     private String title;
 
     @NotNull
-    @ApiModelProperty(position = 3, required = true, dataType = "String", value = "book 배경색")
-    private byte bgColor;
-
-    @NotNull
-    @ApiModelProperty(position = 4, required = true, dataType = "String", value = "삭제여부")
-    private byte isDelete;
+    @ApiModelProperty(position = 3, required = true)
+    private Byte bgColor;
 
     @Builder
-    public BookSaveRequestDto(Long whoseTurn, String title, byte bgColor, byte isDelete) {
+    public BookSaveRequestDto(Long whoseTurn, String title, Byte bgColor) {
         this.whoseTurn = whoseTurn;
         this.title = title;
         this.bgColor = bgColor;
-        this.isDelete = isDelete;
     }
 
     public Book toEntity() {
         return Book.builder()
             .user(new User(whoseTurn))
             .title(title)
-            .inviteCode(UUID.randomUUID().toString())
             .bgColor(bgColor)
-            .isDelete(isDelete)
+            .inviteCode(UUID.randomUUID().toString())
             .build();
     }
 }
