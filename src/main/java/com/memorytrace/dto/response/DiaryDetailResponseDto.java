9a207eeb9bf025ce508a360d3231099ff8e32f63
congcoding <congcoding@gmail.com>
2021-04-15 22:15:04 +0900

    [FEAT] Diary Read API 추가 (#22)

---
+++ src/main/java/com/memorytrace/dto/response/DiaryDetailResponseDto.java
@@ -0,0 +1,44 @@
+package com.memorytrace.dto.response;
+
+import com.memorytrace.domain.Diary;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@ApiModel(value = "Diary 조회 요청")
+public class DiaryDetailResponseDto {
+
+    @ApiModelProperty(position = 1, required = true)
+    private Long did;
+
+    @ApiModelProperty(position = 2, required = true)
+    private String title;
+
+    @ApiModelProperty(position = 3, required = true)
+    private String img;
+
+    @ApiModelProperty(position = 4, required = true)
+    private String content;
+
+    @ApiModelProperty(position = 5, required = true)
+    private Byte alignment;
+
+    @ApiModelProperty(position = 6, required = true)
+    private Byte bgColor;
+
+    @ApiModelProperty(position = 7, required = true)
+    private Byte template;
+
+    public DiaryDetailResponseDto(Diary entity) {
+        this.did = entity.getDid();
+        this.title = entity.getTitle();
+        this.img = entity.getImg();
+        this.content = entity.getContent();
+        this.alignment = entity.getAlignment();
+        this.bgColor = entity.getBgColor();
+        this.template = entity.getTemplate();
+    }
+}
--- src/main/java/com/memorytrace/service/DiaryService.java
+++ src/main/java/com/memorytrace/service/DiaryService.java
@@ -1,5 +1,7 @@
 package com.memorytrace.service;
 
+import com.memorytrace.domain.Diary;
+import com.memorytrace.dto.response.DiaryDetailResponseDto;
 import com.memorytrace.dto.response.DiaryListResponseDto;
 import com.memorytrace.repository.DiaryRepository;
 import java.util.List;
@@ -16,5 +18,12 @@ public class DiaryService {
     @Transactional(readOnly = true)
     public List<DiaryListResponseDto> findByBook_BidOrderByModifiedDateDesc(Long bid) {
         return diaryRepository.findByBook_BidOrderByModifiedDateDesc(bid);
+    }
+
+    @Transactional(readOnly = true)
+    public DiaryDetailResponseDto findByDid(Long did) {
+        Diary entity = diaryRepository.findByDid(did)
+            .orElseThrow(() -> new IllegalArgumentException(("해당 다이어리가 없습니다. did=" + did)));
+        return new DiaryDetailResponseDto(entity);
     }
 }
