cc03d5e551594384bcffff6839eba12dc9f82e7e
congcoding <congcoding@gmail.com>
2021-04-18 00:53:02 +0900

    [FIX] DiaryDetailResponseDto에 nickname 추가 (#22)

--- src/main/java/com/memorytrace/dto/response/DiaryDetailResponseDto.java
+++ src/main/java/com/memorytrace/dto/response/DiaryDetailResponseDto.java
@@ -15,22 +15,26 @@ public class DiaryDetailResponseDto {
     private Long did;
 
     @ApiModelProperty(position = 2, required = true)
-    private String title;
+    private String nickname;
 
     @ApiModelProperty(position = 3, required = true)
-    private String img;
+    private String title;
 
     @ApiModelProperty(position = 4, required = true)
-    private String content;
+    private String img;
 
     @ApiModelProperty(position = 5, required = true)
-    private Byte bgColor;
+    private String content;
 
     @ApiModelProperty(position = 6, required = true)
+    private Byte bgColor;
+
+    @ApiModelProperty(position = 7, required = true)
     private Byte template;
 
     public DiaryDetailResponseDto(Diary entity) {
         this.did = entity.getDid();
+        this.nickname = entity.getUser().getNickname();
         this.title = entity.getTitle();
         this.img = entity.getImg();
         this.content = entity.getContent();
