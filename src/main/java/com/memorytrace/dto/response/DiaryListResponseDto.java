82752d6e4017a894d2208625fef09e20e21ec949
congcoding <congcoding@gmail.com>
2021-04-18 00:49:01 +0900

    [FIX] DiaryListResponseDto에 whoseTurn, nickname 추가 (#22)

--- src/main/java/com/memorytrace/dto/response/DiaryListResponseDto.java
+++ src/main/java/com/memorytrace/dto/response/DiaryListResponseDto.java
@@ -13,23 +13,30 @@ public class DiaryListResponseDto {
     private Long did;
 
     @ApiModelProperty(position = 2, required = true)
-    private String title;
+    private Long whoseTurn;
 
     @ApiModelProperty(position = 3, required = true)
-    private String img;
+    private String nickname;
+
+    @ApiModelProperty(position = 4, required = true)
+    private String title;
 
     @ApiModelProperty(position = 5, required = true)
-    private Byte template;
+    private String img;
 
     @ApiModelProperty(position = 6, required = true)
+    private Byte template;
+
+    @ApiModelProperty(position = 7, required = true)
     private String modifiedDate;
 
     public DiaryListResponseDto(Diary entity) {
         this.did = entity.getDid();
+        this.whoseTurn = entity.getBook().getUser().getUid();
+        this.nickname = entity.getUser().getNickname();
         this.title = entity.getTitle();
         this.img = entity.getImg();
         this.template = entity.getTemplate();
         this.modifiedDate = entity.getModifiedDate().toString();
     }
-
 }
