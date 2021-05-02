65c13864e1dace9696c2b765b58ca4e4cd7c2830
congcoding <congcoding@gmail.com>
2021-05-02 15:22:33 +0900

    [FIX] Diary List READ API에서 whoseTurn을 1번만 불러오도록 수정 (#22)

--- src/main/java/com/memorytrace/dto/response/DiaryListResponseDto.java
+++ src/main/java/com/memorytrace/dto/response/DiaryListResponseDto.java
@@ -3,40 +3,44 @@ package com.memorytrace.dto.response;
 import com.memorytrace.domain.Diary;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @ApiModel(value = "DiaryList 조회 요청")
 public class DiaryListResponseDto {
 
     @ApiModelProperty(position = 1, required = true)
-    private Long did;
-
-    @ApiModelProperty(position = 2, required = true)
     private Long whoseTurn;
 
-    @ApiModelProperty(position = 3, required = true)
-    private String nickname;
+    @ApiModelProperty(position = 2, required = true)
+    private List<DiaryList> diaryList = new ArrayList<>();
 
-    @ApiModelProperty(position = 4, required = true)
-    private String title;
+    @Getter
+    public class DiaryList {
 
-    @ApiModelProperty(position = 5, required = true)
-    private String img;
+        private Long did;
+        private String nickname;
+        private String title;
+        private String img;
+        private Byte template;
+        private String modifiedDate;
 
-    @ApiModelProperty(position = 6, required = true)
-    private Byte template;
+        public DiaryList(Diary entity) {
+            this.did = entity.getDid();
+            this.nickname = entity.getUser().getNickname();
+            this.title = entity.getTitle();
+            this.img = entity.getImg();
+            this.template = entity.getTemplate();
+            this.modifiedDate = entity.getModifiedDate().toString();
+        }
+    }
 
-    @ApiModelProperty(position = 7, required = true)
-    private String modifiedDate;
-
-    public DiaryListResponseDto(Diary entity) {
-        this.did = entity.getDid();
-        this.whoseTurn = entity.getBook().getUser().getUid();
-        this.nickname = entity.getUser().getNickname();
-        this.title = entity.getTitle();
-        this.img = entity.getImg();
-        this.template = entity.getTemplate();
-        this.modifiedDate = entity.getModifiedDate().toString();
+    public DiaryListResponseDto(Long whoseTurn, List<DiaryList> diaryList) {
+        this.whoseTurn = whoseTurn;
+        this.diaryList = diaryList;
     }
 }
