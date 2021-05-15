ceb423117b3ad04d42dde7c3453feed93a56f415
hye0e <nanda5050@naver.com>
2021-05-16 02:22:08 +0900

    [DOCS] Swagger description 추가(#57)

--- src/main/java/com/memorytrace/dto/response/BookListResponseDto.java
+++ src/main/java/com/memorytrace/dto/response/BookListResponseDto.java
@@ -11,31 +11,31 @@ import org.springframework.data.domain.Page;
 
 @Getter
 @NoArgsConstructor
-@ApiModel(value = "BookList 조회 요청")
+@ApiModel(value = "교환 일기장 조회 응답값")
 public class BookListResponseDto extends PageResponseDto {
 
-    @ApiModelProperty(position = 1, required = true)
+    @ApiModelProperty(position = 1, required = true, value = "교환 일기장 리스트")
     private List<BookList> bookList = new ArrayList<>();
 
     @Getter
     public class BookList {
 
-        @ApiModelProperty(position = 1, required = true)
+        @ApiModelProperty(position = 1, required = true, value = "교환 일기장 고유 아이디")
         private Long bid;
 
-        @ApiModelProperty(position = 2, required = true)
+        @ApiModelProperty(position = 2, required = true, value = "현재 차례의 닉네임")
         private String nickname;
 
-        @ApiModelProperty(position = 3, required = true)
+        @ApiModelProperty(position = 3, required = true, value = "교환 일기장 제목")
         private String title;
 
-        @ApiModelProperty(position = 4, required = true)
+        @ApiModelProperty(position = 4, required = true, value = "교환 일기장 배경 색상")
         private Byte bgColor;
 
-        @ApiModelProperty(position = 5)
+        @ApiModelProperty(position = 5, value = "스티커 이미지")
         private String stickerImg;
 
-        @ApiModelProperty(position = 6, required = true)
+        @ApiModelProperty(position = 6, required = true, value = "교환 일기장 수정 날짜")
         private String modifiedDate;
 
         public BookList(UserBook entity) {
--- src/main/java/com/memorytrace/dto/response/UserDetailResponseDto.java
+++ src/main/java/com/memorytrace/dto/response/UserDetailResponseDto.java
@@ -1,13 +1,21 @@
 package com.memorytrace.dto.response;
 
 import com.memorytrace.domain.User;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 @Getter
+@ApiModel(value = "사용지 정보 조회 응답값")
 public class UserDetailResponseDto {
 
+    @ApiModelProperty(position = 1, required = true, value = "사용자 고유 id")
     private Long uid;
+
+    @ApiModelProperty(position = 1, required = true, value = "사용자 닉네임")
     private String nickName;
+
+    @ApiModelProperty(position = 1, required = true, value = "사용자 프로필 사진")
     private String profileImg;
 
     public UserDetailResponseDto(User entity) {
