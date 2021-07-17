9d78d29c3bc6b165987b86b9c33dddcd992e2683
hye0e <nanda5050@naver.com>
2021-07-17 12:23:30 +0900

    [FIX] Fcm Token return 메시지 추가 (#139)

--- src/main/java/com/memorytrace/controller/FcmController.java
+++ src/main/java/com/memorytrace/controller/FcmController.java
@@ -1,5 +1,8 @@
 package com.memorytrace.controller;
 
+import com.memorytrace.common.ResponseMessage;
+import com.memorytrace.common.StatusCode;
+import com.memorytrace.domain.DefaultRes;
 import com.memorytrace.dto.request.FcmDeleteRequestDto;
 import com.memorytrace.service.FcmService;
 import io.swagger.annotations.Api;
@@ -8,6 +11,8 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +31,9 @@ public class FcmController {
         @ApiResponse(code = 200, message = "해당 기기 토큰 지우기 완료")
     })
     @DeleteMapping
-    public void delete(@RequestBody @Valid FcmDeleteRequestDto request) {
+    public ResponseEntity<DefaultRes> delete(@RequestBody @Valid FcmDeleteRequestDto request) {
         fcmService.deletFcmToken(request.getToken());
+        return new ResponseEntity<>(
+            DefaultRes.res(StatusCode.OK, ResponseMessage.DELETE_TOKEN), HttpStatus.OK);
     }
 }
