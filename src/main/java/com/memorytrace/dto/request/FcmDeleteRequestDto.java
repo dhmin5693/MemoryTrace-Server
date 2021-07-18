eb6bce743721c1ed57d4bc0e9f15d8ba9fb92c62
hye0e <nanda5050@naver.com>
2021-07-18 21:18:44 +0900

    [FIX] 토큰 삭제 로직에 jwt 인증 제외 (#180)

--- src/main/java/com/memorytrace/dto/request/FcmDeleteRequestDto.java
+++ src/main/java/com/memorytrace/dto/request/FcmDeleteRequestDto.java
@@ -12,7 +12,11 @@ import lombok.Setter;
 @NoArgsConstructor
 @ApiModel(value = "FCM 토큰 삭제 요청")
 public class FcmDeleteRequestDto {
+    @NotNull(message = "uid값은 필수입니다.")
+    @ApiModelProperty(position = 1, required = true, dataType = "Long", value = "해당 사용자 uid")
+    private Long uid;
+
     @NotNull(message = "token값은 필수입니다.")
-    @ApiModelProperty(position = 1, required = true, dataType = "String", value = "해당 기기 토큰")
+    @ApiModelProperty(position = 2, required = true, dataType = "String", value = "해당 기기 토큰")
     private String token;
 }
